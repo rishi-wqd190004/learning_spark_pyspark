ed52ab8e798e82e6293079eb86ad9b5fa3f5a876
rishi-wqd190004 <wqd190004@siswa.um.edu.my>
2021-02-07 19:56:30 +0800

    adding agregate and practice

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # learning_spark_pyspark
 This is a repository to learn and share more about pyspark and spark applications for ML and Data Science related.
+
+## RDD
+RDD is called as resilient distributed dataset. Mainly its the dataset stored in parallel if few other computers are connected.
+
+## RDD Operations
+Mainly on PySpark we have 2 features
+    - RDD Transformation
+    - RDD Actions
+Will discuss about each of them with code example.
+
+## RDD Transformation
+These are lazy actions and other words you can say its processing part like 'flatMap(), map(), filter(), etc'
+These are mainly 2 types:
+    - Narrow Transformation
+    - Wider Transformation
+
+## Narrow Transformation
+compute data on single partition
+functions like map(), mapPartition(), flatMap(), etc
+![Narrow-transformation](https://i1.wp.com/sparkbyexamples.com/wp-content/uploads/2019/12/narrow-transformation.png?w=545&ssl=1)
+(Source:https://sparkbyexamples.com/apache-spark-rdd/spark-rdd-transformations/) 
+
+## Wider Transformation
+compute data from multiple partitions
+functions like groupByKey(), aggregateByKey(), join(), etc
+![Wider-transformation](https://i0.wp.com/sparkbyexamples.com/wp-content/uploads/2019/12/wider-transformation.png?w=539&ssl=1)
+(Source:https://sparkbyexamples.com/apache-spark-rdd/spark-rdd-transformations/)
--- pyspark_rdd.py
+++ pyspark_rdd.py
@@ -1,7 +1,48 @@
 from pyspark.sql import SparkSession
+from pyspark import SQLContext
 
-spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+spark = SparkSession.builder.appName('spark_learning').getOrCreate() # sets a name for th eapplication and can be seen in Spark Web UI
 
 print(spark)
 rdd = spark.sparkContext.parallelize([1,2,3,4,56])
 print("RDD count: ", rdd.count())
+
+## creating rdd using parallelize()
+dataList = [('Java', 20000), ('Python', 100000), ('Scala', 3000)]
+rdd = spark.sparkContext.parallelize(dataList)
+print('RDD count parallelize: ', rdd.count())
+
+## creating rdd using external file here textFile()
+# rdd2 = spark.sparkContext.textFile('/home/richie/kaggle_datasets/5e15730aa280850006f3d005.txt')
+# print('RDD line count textfile: ', rdd2.count())
+# # get count of Wifi and gyroscope used in lines
+# num_wifi = rdd2.filter(lambda s: 'WIFI' in s).count()
+# num_gyro = rdd2.filter(lambda s: 'GYROSCOPE' in s).count()
+# print('lines with wifi: {}, lines with GYROSCOPE: {}'.format(num_wifi, num_gyro))
+
+## you can create new_RDD from the existing RDD
+
+## RDD Operations
+## RDD Transformation
+# flatmap transformation
+# rdd2_1 = rdd2.flatMap(lambda x: x.splitlines(True))
+# for element in rdd2_1.collect():
+#     with open('/home/richie/kaggle_datasets/dump_data.txt', 'a') as f:
+#         f.writelines(element)
+# # count action
+# print("RDD count: ", rdd2_1.count())
+
+# another example
+data=[("Z", 1),("A", 20),("B", 30),("C", 40),("B", 30),("B", 60)]
+inputRDD = spark.sparkContext.parallelize(data)
+listRDD = spark.sparkContext.parallelize([1,2,3,4,5,3,2]) # here created 2 RDD's
+## aggregate action
+seqOp = (lambda x,y: x+y)
+combOp = (lambda x,y: x+y)
+agg = listRDD.aggregate(0, seqOp, combOp)
+print(agg)
+# another
+seqOp2 = (lambda x, y: (x[0] + y, x[1] + 1))
+combOp2 = (lambda x, y: (x[0] + y[0], x[1] + y[1]))
+agg2 = listRDD.aggregate((0, 0), seqOp2, combOp2)
+print(agg2)
